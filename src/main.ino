d922ad8ef4d72375d40931d73ab4d7f8572cda18
gitlab@runningtoy.de <gitlab@runningtoy.de>
2019-05-08 21:49:18 +0200

    little fixes

--- src/main.ino
+++ src/main.ino
@@ -15,6 +15,7 @@
 #include "myTimer.h"
 #include <RotaryEncoder.h>
 
+
 void nextTrack();
 
 WiFiMulti wifiMulti;
@@ -90,6 +91,94 @@ DFRobotDFPlayerMini myDFPlayer;
 
 const size_t capacity = JSON_OBJECT_SIZE(40) + 20;
 DynamicJsonDocument doc(capacity);
+
+
+
+void WiFiEvent(WiFiEvent_t event)
+{
+    Serial.printf("[WiFi-event] event: %d\n", event);
+
+    switch (event) {
+        case SYSTEM_EVENT_WIFI_READY: 
+            Serial.println("WiFi interface ready");
+            break;
+        case SYSTEM_EVENT_SCAN_DONE:
+            Serial.println("Completed scan for access points");
+            break;
+        case SYSTEM_EVENT_STA_START:
+            Serial.println("WiFi client started");
+            break;
+        case SYSTEM_EVENT_STA_STOP:
+            Serial.println("WiFi clients stopped");
+            break;
+        case SYSTEM_EVENT_STA_CONNECTED:
+            Serial.println("Connected to access point");
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            Serial.println("Disconnected from WiFi access point");
+            break;
+        case SYSTEM_EVENT_STA_AUTHMODE_CHANGE:
+            Serial.println("Authentication mode of access point has changed");
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            Serial.print("Obtained IP address: ");
+            Serial.println(WiFi.localIP());
+            break;
+        case SYSTEM_EVENT_STA_LOST_IP:
+            Serial.println("Lost IP address and IP address is reset to 0");
+            break;
+        case SYSTEM_EVENT_STA_WPS_ER_SUCCESS:
+            Serial.println("WiFi Protected Setup (WPS): succeeded in enrollee mode");
+            break;
+        case SYSTEM_EVENT_STA_WPS_ER_FAILED:
+            Serial.println("WiFi Protected Setup (WPS): failed in enrollee mode");
+            break;
+        case SYSTEM_EVENT_STA_WPS_ER_TIMEOUT:
+            Serial.println("WiFi Protected Setup (WPS): timeout in enrollee mode");
+            break;
+        case SYSTEM_EVENT_STA_WPS_ER_PIN:
+            Serial.println("WiFi Protected Setup (WPS): pin code in enrollee mode");
+            break;
+        case SYSTEM_EVENT_AP_START:
+            Serial.println("WiFi access point started");
+            break;
+        case SYSTEM_EVENT_AP_STOP:
+            Serial.println("WiFi access point  stopped");
+            break;
+        case SYSTEM_EVENT_AP_STACONNECTED:
+            Serial.println("Client connected");
+            break;
+        case SYSTEM_EVENT_AP_STADISCONNECTED:
+            Serial.println("Client disconnected");
+            break;
+        case SYSTEM_EVENT_AP_STAIPASSIGNED:
+            Serial.println("Assigned IP address to client");
+            break;
+        case SYSTEM_EVENT_AP_PROBEREQRECVED:
+            Serial.println("Received probe request");
+            break;
+        case SYSTEM_EVENT_GOT_IP6:
+            Serial.println("IPv6 is preferred");
+            break;
+        case SYSTEM_EVENT_ETH_START:
+            Serial.println("Ethernet started");
+            break;
+        case SYSTEM_EVENT_ETH_STOP:
+            Serial.println("Ethernet stopped");
+            break;
+        case SYSTEM_EVENT_ETH_CONNECTED:
+            Serial.println("Ethernet connected");
+            break;
+        case SYSTEM_EVENT_ETH_DISCONNECTED:
+            Serial.println("Ethernet disconnected");
+            break;
+        case SYSTEM_EVENT_ETH_GOT_IP:
+            Serial.println("Obtained IP address");
+            Serial.println("IP address: ");
+            Serial.println(WiFi.localIP());
+            break;
+    }
+  }
 
 
 
@@ -190,9 +279,9 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
 }
 
 String getIndexHTML(){
-  String ret = F("<!DOCTYPE html><html><body><h2>HTML Links</h2><a href='");
-  ret+=WiFi.localIP();
-  ret+=F("/edit'>Edit Pages</a></body></html>");
+  String ret = F("<!DOCTYPE html><html><body><h2><a href='");
+  //ret+=WiFi.localIP();
+  ret+=F("/edit'>Edit Pages</a></h2></body></html>");
   return ret;
 }
 
@@ -363,30 +452,6 @@ uint8_t getID() {
   return 1;
 }
 
-// //*************** Lautstärkeregelung über Poti KY40  ***************
-
-// void Lauter(){
-//   if (drehen) delay (1); // entprellen
-//   if(digitalRead(CLK) != A) {
-//     A = !A;
-//     if ( A && !B && myDFPlayer.readVolume() != max_Volume)
-//       myDFPlayer.volumeUp();
-//     drehen = false; // entprellen
-//   }
-//   Serial.println("Lauter");
-// }
-
-// void Leiser(){
-//   if (drehen) delay (1);
-//   if(digitalRead(DT) != B) {
-//     B = !B;
-//     if(B && !A && myDFPlayer.readVolume() !=0)
-//       myDFPlayer.volumeDown();
-//     drehen = false;
-//   }
-//   Serial.println("Leiser");
-// }
-
 bool playCard(int id){
     bool ret=false;
     myCard.cardID=id;
@@ -465,17 +530,19 @@ void DFPlayerSetup(){
 }
 
 void WIFISetup(){
-     
+
+    WiFi.onEvent(WiFiEvent);
+    
     //wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
     //wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
     
     Serial.println("Connecting Wifi...");
-    if(wifiMulti.run() == WL_CONNECTED) {
-        Serial.println("");
-        Serial.println("WiFi connected");
-        Serial.println("IP address: ");
-        Serial.println(WiFi.localIP());
-        }
+    // if(wifiMulti.run() == WL_CONNECTED) {
+    //     Serial.println("");
+    //     Serial.println("WiFi connected");
+    //     Serial.println("IP address: ");
+    //     Serial.println(WiFi.localIP());
+    //     }
 
 }
 
@@ -624,7 +691,9 @@ void loop() {
       Serial.println("*******************************");
       Serial.println("Timer abgelaufen, DEEP SLEEP: ");
       Serial.println("*******************************");
-      //esp_deep_sleep_start
+      //myDFPlayer.sleep();
+      //delay(100);
+      //esp_deep_sleep_start();
     }
 
     if (position < encoder.getPosition())
